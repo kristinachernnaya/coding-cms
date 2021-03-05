5e30e1fd51a1d63f55b42f095a0664d967d74d79
kristinachernnaya <79541566+kristinachernnaya@users.noreply.github.com>
2021-03-05 11:18:34 +0200

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -16,6 +16,31 @@
 					<h1 class="page-header">
 						Welcome to the Administration Panel
 					</h1>
+					<div class="col-md-7">
+					<h3>Adaugă utilizator</h3>
+					<form action="validator/validate.php" method="POST" autocomplete="off" >
+					<div class="form-group">
+				<input type="text" name="username" placeholder="Username" id="username" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<input type="text" name="email" placeholder="Email" id="email" class="form-control">
+			</div>
+
+			<div class="form-group">
+				<input type="text" name="password" placeholder="Password" id="password" class="form-control">
+			</div>
+			<div class="form-group">
+			<select name="role" id="role" class="form-control">
+			 <option value="Administrator"> Administrator</option>
+			 <option value="Profesor"> Profesor</option>
+</select>
+			</div>
+            <div class="form-group">
+			<input class="btn btn-success btn-block" type="submit" name="submit" id="submit" value="Register in">
+			</div>
+					</form>
+					</div>
 
 				</div>
 
