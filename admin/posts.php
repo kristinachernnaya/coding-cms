48bef9e33756c78b1c4fa695337f4a83075c3cab
kristinachernnaya <79541566+kristinachernnaya@users.noreply.github.com>
2021-03-05 11:14:53 +0200

    Update posts.php

--- admin/posts.php
+++ admin/posts.php
@@ -1,4 +1,32 @@
-<?php include 'includes/header.php'; ?>
+<?php include 'includes/header.php'; 
+include 'includes/helper.php';
+
+//delete post
+if (isset($_GET['delete_post']) && $_GET['delete_post'] != '') {
+  $dlt = $_GET['delete_post'];
+  if(delete('posts','post_id',$dlt)){
+    redirect('posts.php?source=');
+  }
+}
+
+
+//unapprove post
+if(isset($_GET['statut_post'])&& $_GET['statut_post'] != ''){
+	$app_id = $_GET['statut_post'];
+	if(modifyStatus($app_id)){
+		redirect('posts.php?source=');
+	}
+}
+
+
+//update or modify content
+if(isset($_GET['edit_post'])&& $_GET['edit_post'] != ''){
+$eid = $_GET['edit_post'];
+if(editPost($eid)){
+	echo "okay";
+}
+}
+?>
 
 <divs>
 <div id="wrapper">
@@ -15,26 +43,38 @@
 			<div class="row">
 
 					<h1 class="page-header">
-						Tabla de administrare						
+						Bine ai venit la sistemul de administrare						
 					</h1>
-				
+
+			
+					  
+					<?php
+					global $source;
+						if (isset($_GET['source'])) {
+								$source = $_GET['source'];
+						
+						switch ($source) {
+							    case 'add_new':
+								include "includes/add_post.php";
+								break;
+								case 'edit':
+								include "includes/edit_post.php";
+								break;
+							
+								default:
+								include "includes/view_post.php";
+								break;
+						}
+					}else{
+							include "includes/view_post.php";
+						}						
+		
+					 ?>
 </div>
 				</div>
-
 					 
-			<?php
-			if(isset($_GET['source'])){
-				$source = $_GET['source'];
-			}		 switch ($source) {
-				case 'add_new':
-					include "includes/add_post.php";
-					break;
-				
-				default:
-					include "includes/view_post.php";
-					break;
-			}
-?>
+					 
+
 
 			</div>
 
